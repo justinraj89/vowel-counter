9c2dcc68b530c0e85a871b4eba68089aaa4bb4a7
Justin Rajewski <rajewskijustin@gmail.com>
2023-08-21 19:45:30 -0700

    setup google fonts

---
+++ app/fonts.js
@@ -0,0 +1,12 @@
+import { Barriecito, Cormorant_Garamond } from "@next/font/google";
+
+
+export const barriecito = Barriecito({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+export const cormorantGaramond = Cormorant_Garamond({
+  subsets: ['cyrillic'],
+  weight: '300'
+})
--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
-
+"use client"
+import { useState, useRef } from 'react';
+import { barriecito } from './fonts';
+//========================================
 
 export default function Home() {
   return (
-    <div className="flex flex-col align-center bg-slate-900 h-screen">
-
+    <div className="flex flex-col align-center">
+      <h1 className='text-red-400 md:text-7xl text-5xl text-center mt-14'>Vowel Counter</h1>
+      
     </div>
   )
 }
+
+
+//<h1 className='bg-gradient-to-r from-orange-500 to-pink-400 bg-clip-text text-transparent text-4xl text-center border-2 border-red-400 mt-10'>Vowel Counter</h1>
