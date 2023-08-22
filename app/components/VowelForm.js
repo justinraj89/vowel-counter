dd883c3a3d3e8f0c380d799405d28c9641e8c98f
Justin Rajewski <rajewskijustin@gmail.com>
2023-08-21 22:13:08 -0700

    enhanced form css

--- app/components/VowelForm.js
+++ app/components/VowelForm.js
@@ -7,8 +7,8 @@ function VowelForm({ vowels, vowelInput, setVowelInput }) {
   return (
     <div className="mt-4 w-full lg:w-2/4">
       <form className="w-full flex flex-col md:flex-row justify-center items-center gap-4" onSubmit={countVowels}>
-        <input type="text" className="px-4 w-[90%] lg:w-2/3 py-1 rounded-md font-mono" ref={vowelInput} />
-        <button type="submit" className="text-white bg-indigo-700 px-4 py-1 font-mono rounded-md w-[90%] lg:w-auto">
+        <input type="text" className="w-[90%] text-gray-100 lg:w-2/3 py-1 font-mono bg-transparent border-b-2 border-gray-200 focus:outline-none lg:focus:border-purple-300" ref={vowelInput} placeholder="Enter text here"/>
+        <button type="submit" className="text-white bg-indigo-700 hover:bg-indigo-800 px-4 py-1 font-mono rounded-md w-[90%] lg:w-auto ">
           Submit
         </button>
       </form>
