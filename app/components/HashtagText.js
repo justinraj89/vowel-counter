093ad8752107d0e816ea8da21b2279c6b816e417
Justin Rajewski <rajewskijustin@gmail.com>
2023-08-23 19:35:10 -0700

    click to copy hashtag setup

--- app/components/HashtagText.js
+++ app/components/HashtagText.js
@@ -1,10 +1,31 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import copy from "clipboard-copy";
 
-function HashtagText({hashTagged}) {
+function HashtagText({ hashTagged }) {
+  const [copiedText, setCopiedText] = useState(false);
+
+  const handleCopyText = async () => {
+    await copy(hashTagged);
+    setCopiedText(true);
+    setTimeout(() => setCopiedText(false), 2000); // Reset copied state after 2 seconds
+  };
+
   return (
-    
-    <div>{hashTagged}</div>
-  )
+    <div onClick={handleCopyText} className="">
+      <p className="text-center hover:cursor-pointer font-mono font-bold tracking-wide text-white md:text-5xl text-2xl mt-20 animate__animated animate__fadeInUp">
+        {hashTagged}
+      </p>
+
+      {copiedText && (
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-sm text-black font-mono mt-4 border-2 border-white bg-gray-300 w-fit px-2 text-center">
+            Copied to Clipboard
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default HashtagText
+export default HashtagText;
--- app/hashtagGenerator/page.js
+++ app/hashtagGenerator/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center overflow-hidden p-4">
       <div className="max-w-md md:max-w-2xl">
-        <h1 className="tracking-wide bg-gradient-to-r from-blue-600 via-orange-500 to-yellow-100 bg-clip-text text-transparent md:text-7xl text-5xl text-center mt-4 md:mt-14 animate__animated animate__pulse">
+        <h1 className="tracking-wide bg-gradient-to-r from-red-600 via-orange-500 to-yellow-100 bg-clip-text text-transparent md:text-7xl text-5xl text-center mt-4 md:mt-14 animate__animated animate__pulse">
           #hashtagGenerator
         </h1>
         <h2 className="text-white text-lg md:text-xl font-mono tracking-wide font-bold text-center my-8">
