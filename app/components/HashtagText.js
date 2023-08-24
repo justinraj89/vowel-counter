4ca5f63c40bc31fc1351dc10409695d0ce5163ca
Justin Rajewski <rajewskijustin@gmail.com>
2023-08-23 20:09:17 -0700

    adjusted styling on hashtag screen

--- app/components/HashtagText.js
+++ app/components/HashtagText.js
@@ -8,18 +8,23 @@ function HashtagText({ hashTagged }) {
   const handleCopyText = async () => {
     await copy(hashTagged);
     setCopiedText(true);
-    setTimeout(() => setCopiedText(false), 2000); // Reset copied state after 2 seconds
+    setTimeout(() => setCopiedText(false), 2000); 
   };
 
   return (
-    <div onClick={handleCopyText} className="">
-      <p className="text-center hover:cursor-pointer font-mono font-bold tracking-wide text-white md:text-5xl text-2xl mt-20 animate__animated animate__fadeInUp">
+    <div>
+      <p
+        onClick={handleCopyText}
+        className="text-center hover:cursor-pointer hover:opacity-10 font-mono font-bold tracking-wide bg-gradient-to-r from-yellow-100  to-red-600 bg-clip-text text-transparent md:text-5xl text-2xl mt-20 animate__animated animate__fadeInUp"
+      >
         {hashTagged}
       </p>
-
+      <p className="text-center text-white font-mono mt-4">
+        Click to copy text!
+      </p>
       {copiedText && (
         <div className="flex flex-col justify-center items-center">
-          <p className="text-sm text-black font-mono mt-4 border-2 border-white bg-gray-300 w-fit px-2 text-center">
+          <p className="text-sm text-black font-mono mt-4 border-2 border-white bg-gray-300 w-fit px-2 text-center animate__animated animate__fadeIn">
             Copied to Clipboard
           </p>
         </div>
